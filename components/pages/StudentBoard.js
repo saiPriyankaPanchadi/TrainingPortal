83e396d0902c250e8d512a7ad46592f7a3a85220
saiPriyankaPanchadi <panchadisaipriyanka@gmail.com>
2019-12-23 21:51:52 +0530

    1 file modified

--- components/pages/StudentBoard.js
+++ components/pages/StudentBoard.js
@@ -1,81 +1,119 @@
-import React, { Component } from 'react';
-import {Grid,Cell,Tabs,Tab,} from 'react-mdl';
+import React, { Component } from "react";
+import { Grid, Cell, Tabs, Tab } from "react-mdl";
 
 class StudentBoard extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-    currentUserName: '',
-    courseName: '',
-    activeTab:0 
+      currentUserName: "",
+      courseName: "",
+      activeTab: 0
+    };
   }
-  }
-  
+
   componentDidMount() {
-    const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+    const idToken = JSON.parse(localStorage.getItem("okta-token-storage"));
     this.setState({
       currentUserName: idToken.idToken.claims.name
     });
   }
-  
-  handleSubmit=(e)=> {
+
+  handleSubmit = e => {
     e.preventDefault();
-    
-    this.setState({courseName: e.target.value});
+
+    this.setState({ courseName: e.target.value });
     console.log(this.state.courseName);
+  };
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div>
+          {" "}
+          <h3>UI</h3>
+        </div>
+      );
+    }
+    if (this.state.activeTab === 1) {
+      return (
+        <div>
+          {" "}
+          <h3>Backend</h3>
+        </div>
+      );
+    }
+    if (this.state.activeTab === 2) {
+      return (
+        <div>
+          {" "}
+          <h3>Database</h3>
+        </div>
+      );
+    }
+    if (this.state.activeTab === 3) {
+      return (
+        <div>
+          {" "}
+          <h3>cloud</h3>
+        </div>
+      );
+    }
+    if (this.state.activeTab === 4) {
+      return (
+        <div>
+          {" "}
+          <h3>devops</h3>
+        </div>
+      );
+    }
+    if (this.state.activeTab === 5) {
+      return (
+        <div>
+          {" "}
+          <h3>IOT</h3>
+        </div>
+      );
+    }
   }
 
-toggleCategories () {
-if(this.state.activeTab === 0){
-  return <div> <h3>UI</h3></div>
-} 
-if(this.state.activeTab === 1){
-  return <div> <h3>Backend</h3></div>
-}
-if(this.state.activeTab === 2){
-  return <div> <h3>Database</h3></div>
-}
-if(this.state.activeTab === 3){
-  return <div> <h3>cloud</h3></div>
-}
-if(this.state.activeTab === 4){
-  return <div> <h3>devops</h3></div>
-}
-if(this.state.activeTab === 5){
-  return <div> <h3>IOT</h3></div>
-}
-
-}
-
   render() {
-    const {  currentUserName,courseName } = this.state;
+    const { currentUserName, courseName } = this.state;
     return (
       <div>
-        <h3 className='ui container'>Welcome {currentUserName} on student Portal</h3 >  
-      
-        <div style= {{width :'100%' , margin :'auto'}}>
-        <Grid className='studentsInfo'>
-        <Cell col={12}>
-         <div className="category-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                    <Tab>WebDevelopement/UI</Tab>
-                    <Tab>Backend Technolgies</Tab>
-                    <Tab>Database</Tab>
-                    <Tab> Cloud Computing</Tab>
-                    <Tab> Devops</Tab>
-                    <Tab> IOT</Tab>
-                   
+        <h3 className="ui container">
+          Welcome {currentUserName} on student Portal
+        </h3>
+
+        <div style={{ width: "100%", margin: "auto" }}>
+          <Grid className="studentsInfo">
+            <Cell col={12}>
+              <div className="category-tabs">
+                <Tabs
+                  activeTab={this.state.activeTab}
+                  onChange={tabId => this.setState({ activeTab: tabId })}
+                  ripple
+                >
+                  <Tab>WebDevelopement/UI</Tab>
+                  <Tab>Backend Technolgies</Tab>
+                  <Tab>Database</Tab>
+                  <Tab> Cloud Computing</Tab>
+                  <Tab> Devops</Tab>
+                  <Tab> IOT</Tab>
                 </Tabs>
                 <section>
-                    <div className="study-grid"> 
-                    {this.toggleCategories()}
-                    </div>
+                  <div className="study-grid">
+                    <Grid className="materials">
+                      <Cell col={12}>
+                        <div className="content">
+                          {this.toggleCategories()}
+                        </div>
+                      </Cell>
+                    </Grid>
+                  </div>
                 </section>
-            </div>    
-
-        
-        </Cell>
-        </Grid>
+              </div>
+            </Cell>
+          </Grid>
         </div>
       </div>
     );
