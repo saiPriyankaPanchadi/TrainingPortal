d500ba6326aaff5493e6741ca982b30413b568b1
saiPriyankaPanchadi <panchadisaipriyanka@gmail.com>
2019-12-27 16:07:42 +0530

    5 files modified, 1 deleted

--- components/pages/Tabs/AddCourse.js
+++ components/pages/Tabs/AddCourse.js
@@ -1,14 +1,47 @@
-import React, { Component } from "react";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import React from 'react'
+import Register from '../components/auth/style.css';
 
 
-class AddCourse extends Component {
+class AddCourse extends React.Component {
+  constructor() {
+    super();
+    this.state={
+courseId:'01'
+    }
+  }
+
+  handleSubmit=(event) =>{
+    event.preventDefault();
+    const data = new FormData(event.target);
+    
+    fetch('/api/form-submit-url', {
+      method: 'POST',
+      body: data,
+    });
+  }
+
   render() {
     return (
+     
+       
+      <form className="addCourse" onSubmit={this.handleSubmit}>
       <div>
-      AddCourse
-      </div>
+        <label htmlFor="courseId"> courseId</label>
+        <input id="courseId" name="courseId" type="courseId" value={this.state.courseId} disabled />
+</div>
+        <label htmlFor="courseName">  courseName</label>
+        <input id="courseName" name="courseName" type="courseName"  />
+        
+<br/>
+        <label htmlFor="description">  description</label>
+        <input id="description" name="description" type="description" /> 
+
+        <button primary='btn-bt'>Send data!</button>
+      </form>
+    
+
     );
   }
 }
-export default AddCourse;
+
+export default AddCourse
--- components/pages/Tabs/Courses.js
+++ components/pages/Tabs/Courses.js
@@ -5,16 +5,16 @@ import React, { Component } from "react";
 class Courses extends Component {
     constructor(){
       super();
-      this.items = [
-      {link:1, name:"course1", summary:"Summary Test 1",  description:"Desc 1"},
-      {link:2, name:"course2", summary:"Summary Test 2", description:"Desc 2"},
-      {link:3, name:"course3", summary:"Summary Test 3", description:"Desc 3"},
-    ];
+      this.state={
+        courseID:'01',
+        courseName:''
+      }
     }
   render() {
     return (
-      <div>
-     <h3> CoursesAvailble</h3>
+      <div className='courses'>
+      <h3> Courses Available</h3>
+      
 
       </div>
     );
