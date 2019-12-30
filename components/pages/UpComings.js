21a0bc5ef5e2f3a9089c019c18b5e330f3759dbd
saiPriyankaPanchadi <panchadisaipriyanka@gmail.com>
2019-12-30 12:52:56 +0530

    3 files modified

--- components/pages/UpComings.js
+++ components/pages/UpComings.js
@@ -1,11 +1,45 @@
 import React, { Component } from "react";
+import { FaBookOpen, FaCompass, FaCommentsDollar } from "react-icons/fa";
+import "./style.css";
 
 class UpComings extends Component {
+  state = {
+    UpComings: [
+      {
+        icon: <FaBookOpen />,
+        title: "FreeCourses",
+        info:
+          "online Information Technology courses (IT courses) deliver the basics and beyond in a wide range of topics(trending in market."
+      },
+      {
+        icon: <FaCompass />,
+        title: "onlineIDE",
+        info:
+          "Compile various programming languages online. Add input stream, save output, add notes and tags."
+      },
+      {
+        icon: <FaCommentsDollar />,
+        title: "FreeLancing",
+        info: "intersted on self-employment or to hire software guys."
+      }
+    ]
+  };
   render() {
     return (
-      <div>
+      <section className="services">
         <h4>UpComings</h4>
-      </div>
+        <div className="services-center">
+          {this.state.UpComings.map((item, index) => {
+            return (
+              <article Key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p> {item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
     );
   }
 }
