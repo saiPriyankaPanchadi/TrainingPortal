99bb77695d01da8cfbfa3b420ba45506e1b1f1b5
saiPriyankaPanchadi <panchadisaipriyanka@gmail.com>
2020-01-02 22:37:42 +0530

    1 file modified

--- Context.js
+++ Context.js
@@ -43,6 +43,12 @@ class ProductProvider extends Component {
     product.count =1;
     const price = product.price;
     product.total = price;
+    this.setState (()=> 
+    {
+      return {products : tempProducts,
+      cart :[...this.state.cart,product]};
+
+    }, () => {console.log(this.state)});
   };
 
   render() {
